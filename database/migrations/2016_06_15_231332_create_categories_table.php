5ffc15bee263a401c3782940aac1414c235b9ffa
Nicolás Ormeño <ni.ormeno@gmail.com>
2016-06-15 19:23:53 -0400

    add category model

---
+++ database/migrations/2016_06_15_231332_create_categories_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name', 25)->unique();
+            $table->string('cover', 100)->nullable();
+            $table->smallInteger('posts')->unsigned()->default(0);
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('categories');
+    }
+}
